6bdd5b74b78b2a89f3a5e9e6c4610011b779dd7d
Austin Robinson <austin.rbsn@gmail.com>
2012-07-14 02:04:58 -0400

    Adds xmonad.hs

---
+++ xmonad.hs
@@ -0,0 +1,29 @@
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Layout.NoBorders
+import XMonad.Util.EZConfig
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad 
+        $ withUrgencyHook dzenUrgencyHook { args = ["-bg", "darkgreen"]}
+        $ defaultConfig
+        { 
+            manageHook = manageDocks <+> manageHook defaultConfig,
+            layoutHook = smartBorders $ avoidStruts  $  layoutHook defaultConfig,
+            modMask = mod4Mask,
+            focusFollowsMouse = False,
+            terminal = "lilyterm",
+            focusedBorderColor = "#0000ff",
+            normalBorderColor = "#99ccff"
+        } `additionalKeysP` theseKeys
+            
+theseKeys = 
+    [
+        ("M4-p", spawn "dmenu_run"),
+        ("<XF86AudioRaiseVolume>", spawn "amixer set Master 1+"),
+        ("<XF86AudioLowerVolume>", spawn "amixer set Master 1-"),
+        ("M4-c", spawn "chromium")
+    ]
