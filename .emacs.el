a407f52190699e06e524c7221ff1da13040202f4
Austin Robinson <austin@ycharts.com>
2013-12-27 14:26:18 -0500

    Adds python-mode.el

--- .emacs.el
+++ .emacs.el
@@ -17,7 +17,8 @@
                       solarized-theme
                       find-file-in-repository
                       flycheck
-                      exec-path-from-shell))
+                      exec-path-from-shell
+                      python-mode))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -48,17 +49,16 @@
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
+;; Python
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 (load-theme 'solarized-dark)
