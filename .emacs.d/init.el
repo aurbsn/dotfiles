4be1e14b5815ae55874fac99044c117d94222185
Austin Robinson <austin@signafire.com>
2017-05-15 12:08:40 -0400

    More Clojure+Emacs fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -142,19 +142,5 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(package-selected-packages
-   (quote
-    (cider-eval-sexp-fu cider-decompile neotree tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 (put 'downcase-region 'disabled nil)
