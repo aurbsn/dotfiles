ac1c81a66f85af4f6616ea94428322cbd2ee8295
Austin Robinson <austin@signafire.com>
2017-05-15 11:57:11 -0400

    Untrack packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -150,10 +150,11 @@
  '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (neotree tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
+    (cider-eval-sexp-fu cider-decompile neotree tagedit smex rainbow-delimiters projectile paredit magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)
