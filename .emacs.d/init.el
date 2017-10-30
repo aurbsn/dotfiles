d12a3671c16a6bc9d7fa40dfdcc292fefd9aa933
Austin Robinson <austin@signafire.com>
2017-10-30 16:47:07 -0400

    Trying new color scheme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -161,10 +161,12 @@
  '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (markdown-mode markdown-mode+ restclient company es-mode tagedit smex rainbow-delimiters projectile paredit neotree magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider-eval-sexp-fu cider-decompile))))
+    (green-phosphor-theme parse-csv csv-mode dad-joke markdown-mode markdown-mode+ restclient company es-mode tagedit smex rainbow-delimiters projectile paredit neotree magit ido-ubiquitous exec-path-from-shell clojure-mode-extra-font-locking cider-eval-sexp-fu cider-decompile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(global-set-key (kbd "<f5>") 'kmacro-end-or-call-macro)
