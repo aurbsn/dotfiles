b8e2c1be886ba47391cca0a2671267f1410c0768
Austin Robinson <austin@signafire.com>
2018-05-07 16:35:48 -0400

    modernize config a bit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -57,7 +57,9 @@
     ;; allow ido usage in as many contexts as possible. see
     ;; customizations/navigation.el line 23 for a description
     ;; of ido
-    ido-ubiquitous
+    ido-completing-read+
+
+    dracula-theme
 
     ;; Enhances M-x to allow easier execution of commands. Provides
     ;; a filterable list of possible commands in the minibuffer
