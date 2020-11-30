6b701cba94a3e5993e9db034cbd0fcdb13ba77ad
Austin Robinson <arbn@arbn.nyc>
2020-11-30 02:18:54 -0500

    Eshell fixes

--- .emacs.d/customizations/setup-eshell.el
+++ .emacs.d/customizations/setup-eshell.el
@@ -1,2 +1,2 @@
-(add-to-list 'eshell-visual-subcommands '("git" "log"))
-(add-to-list 'eshell-visual-subcommands '("git" "diff"))
+(add-to-list 'eshell-visual-subcommands '("git" "log" "diff" "show"))
+(setq eshell-destroy-buffer-when-process-dies 't)
