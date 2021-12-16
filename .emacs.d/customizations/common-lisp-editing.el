9662f26318fe85f806ad26c430711d03d2f07f73
Austin <arbn@arbn.nyc>
2021-12-16 02:05:57 -0500

    Adds clhs-use-local.el

--- .emacs.d/customizations/common-lisp-editing.el
+++ .emacs.d/customizations/common-lisp-editing.el
@@ -1,6 +1,8 @@
+;; Quicklisp
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
+(load "/home/arbn/quicklisp/clhs-use-local.el" t)
+
 ;; Replace "sbcl" with the path to your implementation
 (setq inferior-lisp-program (executable-find "sbcl"))
-
 (require 'slime-autoloads)
 
