b74d77c4de31c522134a5ae823b4a95b3f45261b
Austin Robinson <arbn@arbn.nyc>
2022-02-24 18:51:24 -0500

    Increase emacs font size and start guix-home

---
+++ guix-config/home-configuration.scm
@@ -0,0 +1,38 @@
+;; This "home-environment" file can be passed to 'guix home reconfigure'
+;; to reproduce the content of your profile.  This is "symbolic": it only
+;; specifies package names.  To reproduce the exact same profile, you also
+;; need to capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(use-modules
+  (gnu home)
+  (gnu packages)
+  (gnu services)
+  (guix gexp)
+  (gnu home services shells))
+
+(home-environment
+  (packages
+    (map (compose list specification->package+output)
+         (list "mu"
+               "guile"
+               "fontconfig"
+               "font-gnu-freefont"
+               "font-dejavu"
+               "font-ghostscript"
+               "glibc-locales"
+               "sbcl"
+               "acpi"
+               "font-adobe-source-code-pro")))
+  (services
+    (list (service
+            home-bash-service-type
+            (home-bash-configuration
+              (bashrc
+               (list (local-file
+                      "dev/dotfiles/guix-config/.bashrc"
+                      "bashrc")))
+              (bash-logout
+               (list (local-file
+                      "dev/dotfiles/guix-config/.bash_logout"
+                      "bash_logout")))))))
