1bb42b05c03f4e1b06825bb409ced7ee7844af00
Austin Robinson <arbn@arbn.nyc>
2022-04-10 23:38:01 -0400

    mu4e config

--- .emacs.d/customizations/setup-mu4e.el
+++ .emacs.d/customizations/setup-mu4e.el
@@ -6,15 +6,27 @@
 
 ;; use mu4e for e-mail in emacs
 (setq mail-user-agent 'mu4e-user-agent)
-(setq mu4e-get-mail-command "mbsync")
 
 (setq mu4e-sent-folder   "/sent")
 (setq mu4e-drafts-folder "/drafts")
 (setq mu4e-trash-folder  "/trash")
 
-;; smtp mail setting; these are the same that `gnus' uses.
-(setq
-   message-send-mail-function   'smtpmail-send-it
-   smtpmail-default-smtp-server "127.0.0.1"
-   smtpmail-smtp-server         "127.0.0.1"
-   smtpmail-local-domain        "127.0.0.1")
+(setq mu4e-maildir "~/mail"
+      mu4e-attachment-dir "~/downloads")
+
+(setq user-mail-address "arbn@arbn.nyc"
+      user-full-name  "Austin Robinson")
+
+;; Get mail
+(setq mu4e-get-mail-command "mbsync protonmail"
+      mu4e-change-filenames-when-moving t   ; needed for mbsync
+      mu4e-update-interval 120)             ; update every 2 minutes
+
+;; Send mail
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-auth-credentials "~/.authinfo.gpg"
+      smtpmail-smtp-server "127.0.0.1"
+      smtpmail-stream-type 'starttls
+      smtpmail-smtp-service 1025)
+
+(add-to-list 'gnutls-trustfiles (expand-file-name "~/.config/protonmail/bridge/cert.pem"))
