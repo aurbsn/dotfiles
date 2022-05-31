da1c7a96a631ad8fe40954e989c49c68d81690d8
Austin Robinson <arbn@arbn.nyc>
2022-05-31 13:03:19 -0400

    rust-mode stuff

--- guix-config/config-files/emacs.d/customizations/setup-rs.el
+++ guix-config/config-files/emacs.d/customizations/setup-rs.el
@@ -1,1 +1,5 @@
 (setq rust-format-on-save t)
+(add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
+;(define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
+(add-hook 'rust-mode-hook #'lsp)
