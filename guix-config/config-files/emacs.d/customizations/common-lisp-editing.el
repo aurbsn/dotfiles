cedc61ae3d704bbefbb1ff942db9a3a879e58760
Austin Robinson <arbn@arbn.nyc>
2022-06-21 02:03:25 +0000

    StumpWM on Arch fixes

--- guix-config/config-files/emacs.d/customizations/common-lisp-editing.el
+++ guix-config/config-files/emacs.d/customizations/common-lisp-editing.el
@@ -1,7 +1,4 @@
-;; Quicklisp
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(load "/home/arbn/quicklisp/clhs-use-local.el" t)
-
+(require 'slime)
 (setq inferior-lisp-program (executable-find "sbcl"))
 (require 'slime-autoloads)
 (slime-setup '(slime-company))
--- guix-config/config-files/emacs.d/customizations/setup-mu4e.el
+++ guix-config/config-files/emacs.d/customizations/setup-mu4e.el
@@ -1,12 +1,10 @@
 ;; example configuration for mu4e
 
 ;; make sure mu4e is in your load-path
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
 (require 'mu4e)
 
 ;; use mu4e for e-mail in emacs
 (setq mail-user-agent 'mu4e-user-agent)
-
 (setq mu4e-sent-folder   "/sent")
 (setq mu4e-drafts-folder "/drafts")
 (setq mu4e-trash-folder  "/trash")
