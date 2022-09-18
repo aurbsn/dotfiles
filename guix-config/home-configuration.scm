30b8e41fbec3fdd9137596d33ba7633d46761aff
Austin Robinson <arbn@arbn.nyc>
2022-09-17 22:15:18 -0400

    Adjust

--- guix-config/home-configuration.scm
+++ guix-config/home-configuration.scm
@@ -37,24 +37,17 @@
      (simple-service 'home-config
       home-files-service-type
       (list `(".config/guix/channels.scm"
-              ,(scheme-file "channels.scm" '(cons* (channel
-                                                    (name 'flat)
-                                                    (url "https://github.com/flatwhatson/guix-channel.git")
-                                                    (introduction
-                                                     (make-channel-introduction
-                                                      "33f86a4b48205c0dc19d7c036c85393f0766f806"
-                                                      (openpgp-fingerprint
-                                                       "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
-                                                   (channel
-                                                    (name 'nonguix)
-                                                    (url "https://gitlab.com/nonguix/nonguix")
-                                                    ;; Enable signature verification:
-                                                    (introduction
-                                                     (make-channel-introduction
-                                                      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-                                                      (openpgp-fingerprint
-                                                       "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-                                                   %default-channels)))
+              ,(scheme-file "channels.scm" '(cons* 
+                                             (channel
+                                              (name 'nonguix)
+                                              (url "https://gitlab.com/nonguix/nonguix")
+                                              ;; Enable signature verification:
+                                              (introduction
+                                               (make-channel-introduction
+                                                "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                                                (openpgp-fingerprint
+                                                 "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+                                             %default-channels)))
             `(".xsession"
               ,(local-file "config-files/xinitrc" #:recursive? #t))
             `(".xinitrc"
@@ -87,7 +80,7 @@
                "keepassxc"
                "acpi"
                "font-adobe-source-code-pro"
-               "emacs-native-comp"
+               "emacs"
                "emacs-vterm"
                "nyxt"
                "unzip"
@@ -109,7 +102,7 @@
                "sbcl-stumpwm-ttf-fonts"
                "cl-fiveam"
                "qt5ct"
-               "hledger"
+               ;"hledger"
                "emacs-hledger-mode"
                "xlsx2csv"
                "git"
