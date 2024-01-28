657edcad3751fc67a649208a3bcfdef561ea660d
Austin C Robinson <arbn@arbn.nyc>
2024-01-28 18:47:30 +0800

    Guix updates

--- guix-config/home-configuration.scm
+++ guix-config/home-configuration.scm
@@ -48,42 +48,19 @@
                                                 (openpgp-fingerprint
                                                  "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
                                              %default-channels)))
-            `(".config/common-lisp/source-registry.conf.d/arbn-lisp.conf"
-              ,(local-file "config-files/arbn-lisp.conf"))
-            `(".xsession"
-              ,(local-file "config-files/xinitrc" #:recursive? #t))
-            `(".xinitrc"
-              ,(local-file "config-files/xinitrc" #:recursive? #t))
             `(".emacs.d/init.el"
              ,(local-file "config-files/emacs.d/init.el" #:recursive? #t))
             `(".emacs.d/customizations"
               ,(local-file "config-files/emacs.d/customizations" #:recursive? #t))
 	    `(".emacs.d/env"
 	      ,(local-file "config-files/emacs.d/env" #:recursive? #t))
-            `(".stumpwm.d/init.lisp"
-             ,(local-file "config-files/stumpwm.d/init.lisp" #:recursive? #t))
-            `(".stumpwm.d/modules"
-             ,(local-file "config-files/stumpwm.d/modules" #:recursive? #t))
-            `("dev/start-stump.lisp"
-              ,(local-file "config-files/start-stump.lisp"))
             `(".sbclrc"
-              ,(local-file "config-files/sbclrc"))
-            `(".mbsyncrc"
-              ,(local-file "config-files/mbsyncrc"))
-            `(".screenlayout/docked.sh"
-              ,(local-file "config-files/docked.sh" #:recursive? #t))
-            `(".config/nyxt/init.lisp" 
-              ,(local-file "config-files/nyxt/init.lisp"))
-            `(".Xresources" 
-              ,(local-file "config-files/Xresources"))))))
+              ,(local-file "config-files/sbclrc"))))))
   (packages
     (map (compose list specification->package+output)
-         (list "mu"
-               "isync"
-               "guile"
+         (list "guile"
                "xclip"
                "sbcl"
-               "acpi"
                "font-adobe-source-code-pro"
                "emacs"
                "emacs-vterm"
@@ -92,36 +69,23 @@
                "libgnome-keyring"
                "glibc-locales"
                "nss-certs"
-               "fcitx5"
-               "fcitx5-configtool"
-               "fcitx5-chinese-addons"
-               "fcitx5-gtk"
-               "fcitx5-qt"
                "font-wqy-zenhei"
                "font-wqy-microhei"
                "font-adobe-source-han-sans:cn" 
                "xlsfonts"
                "syncthing"
                "cmake"
-               "cl-stumpwm"
                "cl-slime-swank"
-               "cl-clx-truetype"
                "cl-drakma"
                "cl-fiveam"
                "cl-json"
-               "qt5ct"
-               "hledger"
-               "emacs-hledger-mode"
                "xlsx2csv"
                "git"
                "exercism"
                "libreoffice"
-               "autorandr"
                "borg"
                "borgmatic"
                "oath-toolkit"
-               "password-store"
-               "arandr"
 
                ;; gstreamer plugins
                "gstreamer"
