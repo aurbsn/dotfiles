6e779d6c6f76026a16dfa375ee590444d7778fed
Austin Robinson <arobinson@etsy.com>
2024-08-15 14:03:14 -0400

    Environment updates

--- guix-config/config-files/emacs.d/customizations/setup-scala.el
+++ guix-config/config-files/emacs.d/customizations/setup-scala.el
@@ -1,10 +1,3 @@
-;; Enable defer and ensure by default for use-package
-;; Keep auto-save/backup files separate from source code:  https://github.com/scalameta/metals/issues/1027
-(setq use-package-always-defer t
-      use-package-always-ensure t
-      backup-directory-alist `((".*" . ,temporary-file-directory))
-      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
-
 ;; Enable scala-mode for highlighting, indentation and motion commands
 (use-package scala-mode
   :interpreter
@@ -23,7 +16,3 @@
    ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
    (setq sbt:program-options '("-Dsbt.supershell=false"))
 )
-
-;; Enable nice rendering of diagnostics like compile errors.
-(use-package flycheck
-  :init (global-flycheck-mode))
