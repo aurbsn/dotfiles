f0642540402539aae885230dffd19762db01697e
Austin Robinson <arbn@arbn.nyc>
2024-08-23 19:52:21 -0400

    guix system update for desktop

--- guix-config/desktop-config.scm
+++ guix-config/desktop-config.scm
@@ -14,16 +14,13 @@
 
  (mapped-devices (list (mapped-device
                         (source (uuid
-                                 "42fe484e-c7f2-46a9-935f-056f95d6504a"))
+                                 "c300b946-fd97-4dc1-b4ab-b2fd7cdb2891"))
                         (target "cryptroot")
                         (type luks-device-mapping))))
 
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
-  (file-systems (cons* (file-system
+ (file-systems (cons* (file-system
                          (mount-point "/boot/efi")
-                         (device (uuid "323C-FB97"
+                         (device (uuid "F1DB-22A3"
                                        'fat32))
                          (type "vfat"))
                        (file-system
