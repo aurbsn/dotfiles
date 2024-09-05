1884596da97fdee600dbc273ba19e4b259b8a15b
Austin C Robinson <arbn@arbn.nyc>
2024-09-05 14:02:09 -0400

    Indentation

--- guix-config/systems/arbn-desktop.scm
+++ guix-config/systems/arbn-desktop.scm
@@ -22,8 +22,7 @@
 
 (system-config
  #:system
- (
-  (kernel-arguments '("modprobe.blacklist=nouveau"
+ ((kernel-arguments '("modprobe.blacklist=nouveau"
                       ;; Set this if the card is not used for displaying or
                       ;; you're using Wayland:
                       "nvidia_drm.modeset=1"))
@@ -115,44 +114,46 @@
                      (service home-pipewire-service-type)
                      (service home-dbus-service-type)
 
-                                        ; Configuration files
-                     (simple-service 'home-config
-                                     home-files-service-type
-                                     (list `(".config/guix/channels.scm"
-                                             ,(scheme-file "channels.scm" '(cons* 
-                                                                            (channel
-                                                                             (name 'nonguix)
-                                                                             (url "https://gitlab.com/nonguix/nonguix")
-                                                                             ;; Enable signature verification:
-                                                                             (introduction
-                                                                              (make-channel-introduction
-                                                                               "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-                                                                               (openpgp-fingerprint
-                                                                                "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-                                                                            (channel
-                                                                             (name 'rosenthal)
-                                                                             (url "https://codeberg.org/hako/rosenthal.git")
-                                                                             (branch "trunk")
-                                                                             (introduction
-                                                                              (make-channel-introduction
-                                                                               "7677db76330121a901604dfbad19077893865f35"
-                                                                               (openpgp-fingerprint 
-                                                                                "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
-                                                                            %default-channels)))
-                                           `(".emacs.d/init.el"
-                                             ,(local-file "config-files/emacs.d/init.el" #:recursive? #t))
-                                           `(".emacs.d/customizations"
-                                             ,(local-file "config-files/emacs.d/customizations" #:recursive? #t))
-	                                   `(".emacs.d/env"
-	                                     ,(local-file "config-files/emacs.d/env" #:recursive? #t))
-                                           `(".sbclrc"
-                                             ,(local-file "config-files/sbclrc"))
-                                           `("bin/wrappedhl.sh"
-                                             ,(local-file "config-files/wrappedhl.sh"))
-                                           `(".config/waybar/config"
-                                             ,(local-file "config-files/waybar/conf"))
-                                           `(".config/hypr/hyprland.conf"
-                                             ,(local-file "config-files/hyprland.conf"))))))
+                     ; Configuration files
+                     (simple-service 
+                      'home-config
+                      home-files-service-type
+                      (list `(".config/guix/channels.scm"
+                              ,(scheme-file "channels.scm" 
+                                            '(cons* 
+                                              (channel
+                                               (name 'nonguix)
+                                               (url "https://gitlab.com/nonguix/nonguix")
+                                               ;; Enable signature verification:
+                                               (introduction
+                                                (make-channel-introduction
+                                                 "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                                                 (openpgp-fingerprint
+                                                  "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+                                              (channel
+                                               (name 'rosenthal)
+                                               (url "https://codeberg.org/hako/rosenthal.git")
+                                               (branch "trunk")
+                                               (introduction
+                                                (make-channel-introduction
+                                                 "7677db76330121a901604dfbad19077893865f35"
+                                                 (openpgp-fingerprint 
+                                                  "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
+                                              %default-channels)))
+                            `(".emacs.d/init.el"
+                              ,(local-file "config-files/emacs.d/init.el" #:recursive? #t))
+                            `(".emacs.d/customizations"
+                              ,(local-file "config-files/emacs.d/customizations" #:recursive? #t))
+	                    `(".emacs.d/env"
+	                      ,(local-file "config-files/emacs.d/env" #:recursive? #t))
+                            `(".sbclrc"
+                              ,(local-file "config-files/sbclrc"))
+                            `("bin/wrappedhl.sh"
+                              ,(local-file "config-files/wrappedhl.sh"))
+                            `(".config/waybar/config"
+                              ,(local-file "config-files/waybar/conf"))
+                            `(".config/hypr/hyprland.conf"
+                              ,(local-file "config-files/hyprland.conf"))))))
                    (packages
                     (map (compose list specification->package+output)
                          (list 
