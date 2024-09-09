d6d09838efa9902e317956ccfbcd176b39a4b01e
Austin Robinson <arbn@arbn.nyc>
2024-09-09 18:43:21 -0400

    Refactor

---
+++ guix-config/arbn/modules/package-lists.scm
@@ -0,0 +1,37 @@
+(define-module (arbn modules package-lists)
+  #:use-module (gnu))
+(use-package-modules 
+ base guile package-management terminals xdisorg freedesktop text-editors 
+ version-control file-systems gnome certs lisp lisp-check emacs
+ emacs-xyz education rust rust-apps fonts linux)
+
+(define-public %base-home-packages ; These packages will always be desired for Guix Home configs
+  (list
+   git
+   nss-certs
+   glibc
+   (list glibc "static")
+   glibc-locales
+   guile-3.0
+   xclip
+   sbcl
+   cl-fiveam
+   emacs
+   emacs-vterm
+   exercism
+   rust
+   rust-cargo
+   font-adobe-source-code-pro
+   ))
+
+(define-public %base-system-packages ; All systems need these
+  (append
+   (list
+    glibc-locales
+    mg
+    git
+    exfat-utils
+    fuse-exfat
+    gvfs ;; Enable user mounts
+    )
+   %base-packages))
---
+++ guix-config/arbn/modules/service-lists.scm
@@ -0,0 +1,73 @@
+(define-module (arbn modules service-lists)
+  #:use-module (gnu)
+  #:use-module (gnu services)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shells))
+(use-service-modules guix cups desktop networking ssh xorg avahi dbus sound pm)
+
+(define-public (create-home-services my-services my-files)
+  (list
+   (service
+    home-bash-service-type
+    (home-bash-configuration
+     (bash-profile 
+      (list (local-file
+             "../../config-files/bash_profile"
+             "bash_profile")))
+     (bashrc
+      (list (local-file
+             "../../config-files/bashrc"
+             "bashrc")))
+     (bash-logout
+      (list (local-file
+             "../../config-files/bash_logout"
+             "bash_logout")))))
+
+   ; Configuration files
+   (simple-service 
+    'home-config
+    home-files-service-type
+    (list `(".config/guix/channels.scm"
+            ,(scheme-file "channels.scm" '
+                          (cons* 
+                           (channel
+                            (name 'nonguix)
+                            (url "https://gitlab.com/nonguix/nonguix")
+                            ;; Enable signature verification:
+                            (introduction
+                             (make-channel-introduction
+                              "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                              (openpgp-fingerprint
+                               "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+                           
+                           (channel
+                            (name 'rosenthal)
+                            (url "https://codeberg.org/hako/rosenthal.git")
+                            (branch "trunk")
+                            (introduction
+                             (make-channel-introduction
+                              "7677db76330121a901604dfbad19077893865f35"
+                              (openpgp-fingerprint 
+                               "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
+                           %default-channels)))
+          `(".emacs.d/init.el"
+            ,(local-file "../../config-files/emacs.d/init.el" #:recursive? #t))
+          `(".emacs.d/customizations"
+            ,(local-file "../../config-files/emacs.d/customizations" #:recursive? #t))
+          `(".emacs.d/env"
+	    ,(local-file "../../config-files/emacs.d/env" #:recursive? #t))
+          `(".sbclrc"
+            ,(local-file "../../config-files/sbclrc"))))))
+
+(define-public (create-system-services my-services)
+  (modify-services 
+      my-services
+      (guix-service-type config => 
+                         (guix-configuration
+                          (inherit config)
+                          (substitute-urls
+                           (append (list "https://substitutes.nonguix.org")
+                                   %default-substitute-urls))
+                          (authorized-keys
+                           (append (list (local-file "../../signing-key.pub"))
+                                   %default-authorized-guix-keys))))))
