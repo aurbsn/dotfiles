f3a47def79e1933b5d4c906b43a6c8107cfb5f76
Austin Robinson <arbn@arbn.nyc>
2025-01-22 16:04:40 -0500

    Fix setup-gtp.el

--- guix-config/config-files/emacs.d/customizations/setup-gpt.el
+++ guix-config/config-files/emacs.d/customizations/setup-gpt.el
@@ -1,8 +1,6 @@
 (use-package gptel)
-(gptel-make-anthropic "Claude"          ;Any name you want
-  :stream t                             ;Streaming responses
-  :key "[REDACTED-API-KEY]")
 (setq
- gptel-model 'claude-3-sonnet-20240229 ;  "claude-3-opus-20240229" also available
- gptel-backend (gptel-make-anthropic "Claude"
-                 :stream t :key "claude-3-5-sonnet-20241022"))
+ gptel-model 'claude-3-sonnet-20240229 ;
+ gptel-backend (gptel-make-anthropic "Claude" ; Any name you want
+                 :stream t ; Streaming responses
+                 :key "[REDACTED-API-KEY]"))
