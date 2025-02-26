83e58788123a444d4f3eba95ffcfed3767e2622b
Austin Robinson <arbn@arbn.nyc>
2025-02-26 16:34:58 -0500

    gptel stuff

--- guix-config/config-files/emacs.d/customize.el
+++ guix-config/config-files/emacs.d/customize.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vterm gameoflife paredit rainbow-delimiters clojure-mode php-mode clojure-mode-extra-font-locking cider aggressive-indent sly sly-asdf sly-quicklisp sly-repl-ansi-color cyberpunk-theme projectile rainbow-delimiters tagedit magit web-mode haskell-mode rust-mode elfeed use-package nov pdf-tools yasnippet mustache-mode s f sicp org-roam exec-path-from-shell guix clhs eglot geiser geiser-guile restclient quelpa kkp corfu counsel consult gpt)))
+   '(terraform-mode vterm gameoflife paredit rainbow-delimiters clojure-mode php-mode clojure-mode-extra-font-locking cider aggressive-indent sly sly-asdf sly-quicklisp sly-repl-ansi-color cyberpunk-theme projectile rainbow-delimiters tagedit magit web-mode haskell-mode rust-mode elfeed use-package nov pdf-tools yasnippet mustache-mode s f sicp org-roam exec-path-from-shell guix clhs eglot geiser geiser-guile restclient quelpa kkp corfu counsel consult gpt)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
