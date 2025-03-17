d8b09d9f8520391338bd902bf5920e060b4fe78d
Austin Robinson <arbn@arbn.nyc>
2025-03-17 15:06:03 -0400

    Aider remove auto-commit

--- guix-config/config-files/emacs.d/customizations/setup-gpt.el
+++ guix-config/config-files/emacs.d/customizations/setup-gpt.el
@@ -13,8 +13,6 @@
   (global-set-key (kbd "C-c a") 'aidermacs-transient-menu)
   ; Enable minor mode for Aider files
   (aidermacs-setup-minor-mode)
-  ; See the Configuration section below
-  (setq aidermacs-auto-commits t)
   (setq aidermacs-use-architect-mode t)
   ; Ensure emacs can access *_API_KEY through .bashrc or setenv
   (setenv "ANTHROPIC_API_KEY" anthropic-api-key)
