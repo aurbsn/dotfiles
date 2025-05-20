13cbfa71da2525cd35458fb7389eba0e59673ac2
Austin Robinson <arbn@arbn.nyc>
2025-05-20 15:24:26 -0400

    Move to guix for emacs packages

--- guix-config/config-files/emacs.d/customizations/setup-gpt.el
+++ guix-config/config-files/emacs.d/customizations/setup-gpt.el
@@ -6,16 +6,3 @@
  gptel-backend (gptel-make-anthropic "Claude" ; Any name you want
                  :stream t ; Streaming responses
                  :key vody-anthropic-api-key))
-
-(use-package aidermacs
-  :straight (:host github :repo "MatthewZMD/aidermacs" :files ("*.el"))
-  :config
-  (setq aidermacs-default-model "sonnet")
-  (global-set-key (kbd "C-c a") 'aidermacs-transient-menu)
-  ; Enable minor mode for Aider files
-  (aidermacs-setup-minor-mode)
-  (setq aidermacs-use-architect-mode t)
-  ; Ensure emacs can access *_API_KEY through .bashrc or setenv
-  (setenv "ANTHROPIC_API_KEY" vody-anthropic-api-key)
-  (setq aidermacs-backend 'vterm)
-  (setq aidermacs-vterm-multiline-newline-key "S-<return>"))
