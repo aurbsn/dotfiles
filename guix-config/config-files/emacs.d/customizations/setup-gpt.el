f55e5743d530005aa564f64ff6d8f54aa361cef4
Austin Robinson <arbn@arbn.nyc>
2025-07-11 18:13:23 -0400

    Work stuff

--- guix-config/config-files/emacs.d/customizations/setup-gpt.el
+++ guix-config/config-files/emacs.d/customizations/setup-gpt.el
@@ -6,3 +6,11 @@
  gptel-backend (gptel-make-anthropic "Claude" ; Any name you want
                  :stream t ; Streaming responses
                  :key vody-anthropic-api-key))
+(use-package claude-code :ensure t
+  :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
+  :config (claude-code-mode)
+  :bind-keymap ("C-c c" . claude-code-command-map))
+(setq claude-code-terminal-backend 'vterm)
+
+
+
