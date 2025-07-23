3760d60eb212a93e767fb8d567ef8f19dffb26bd
Austin Robinson <157371231+aurbsn@users.noreply.github.com>
2025-07-23 00:26:01 -0400

    Update setup-gpt.el

--- guix-config/config-files/emacs.d/customizations/setup-gpt.el
+++ guix-config/config-files/emacs.d/customizations/setup-gpt.el
@@ -1,11 +1,4 @@
 (use-package gptel)
-(setq anthropic-api-key "[REDACTED-API-KEY]")
-(setq vody-anthropic-api-key "[REDACTED-WORK-API-KEY]")
-(setq
- gptel-model 'claude-3-sonnet-20241022 ;
- gptel-backend (gptel-make-anthropic "Claude" ; Any name you want
-                 :stream t ; Streaming responses
-                 :key vody-anthropic-api-key))
 (use-package claude-code :ensure t
   :vc (:url "https://github.com/stevemolitor/claude-code.el" :rev :newest)
   :config (claude-code-mode)
