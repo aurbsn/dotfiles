965150fa8677b93d7735823a269848acae151f7d
Austin Robinson <arbn@arbn.nyc>
2025-09-29 08:57:01 +0800

    arbn-dev system

--- guix-config/arbn/systems/base-system.scm
+++ guix-config/arbn/systems/base-system.scm
@@ -25,7 +25,7 @@
                   (group "users")
                   (home-directory "/home/arbn")
                   (supplementary-groups
-                   '("wheel" "netdev" "audio" "video" "plugdev" "libvirt")))
+                   '("wheel" "netdev" "audio" "video")))
                  %base-user-accounts))
 
    (services
