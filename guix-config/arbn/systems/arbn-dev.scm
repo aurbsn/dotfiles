94b8b28ce30368e894bf9369aba39f25de2c8f30
Austin Robinson <arbn@arbn.nyc>
2025-10-02 18:41:39 -0400

    Still building arbn-dev system config

--- guix-config/arbn/systems/arbn-dev.scm
+++ guix-config/arbn/systems/arbn-dev.scm
@@ -6,9 +6,17 @@
   #:use-module (nongnu system linux-initrd)
   #:use-module (nongnu packages chrome)
   #:use-module (nongnu packages linux)
-  #:use-module (gnu home))
-(use-service-modules desktop networking ssh xorg)
-(use-package-modules audio curl networking pulseaudio linux wm xorg)
+  #:use-module (gnu home)
+  #:use-module (gnu home services syncthing)
+  #:use-module (gnu home services sound)
+  #:use-module (gnu home services desktop))
+(use-service-modules desktop networking ssh xorg syncthing dbus security-token)
+(use-package-modules audio curl networking pulseaudio linux wm xorg gnome web-browsers security-token)
+
+(define %fido2-rule
+  (udev-rule
+   "90-fido2.rules"
+   (string-append "KERNEL==\"hidraw*\", SUBSYSTEM==\"hidraw\", ATTRS{idProduct}==\"0407\", GROUP=\"plugdev\", ATTRS{idVendor}==\"1050\" TAG+=\"uaccess\"" "\n")))
 
 (system-config
  #:system
@@ -48,25 +56,35 @@
   
   (packages
    (append (list
-            blueman
-            bluez
-            bluez-alsa
-            pulseaudio
-            curl)
+            gnome-tweaks
+            gnome-shell-extensions)
            %base-system-packages)))
  #:home
  (home-environment 
   (services
    (create-home-services 
-    '() 
+    (list ; System-specific home services
+     (service home-syncthing-service-type)
+     (service home-pipewire-service-type)
+     (service home-dbus-service-type))
     '()
     #:free #f))
   (packages
-   %desktop-home-packages))
+   (append
+    (list 
+     nyxt)
+    %desktop-home-packages)))
  #:my-system-services
  (create-system-services 
  (append 
   (list 
+   (service bluetooth-service-type
+            (bluetooth-configuration
+             (auto-enable? #t)))
+   ; Smart Cards (Yubikey)
+  (service pcscd-service-type)
+  (udev-rules-service 'fido2 libfido2 #:groups '("plugdev"))
+  (udev-rules-service 'u2f %fido2-rule #:groups '("plugdev"))
    (service gnome-desktop-service-type))
   (modify-services 
    %desktop-services
