591977f75baf7b8006236bfe0c8a77a0f522cfb0
Austin Robinson <arbn@arbn.nyc>
2025-12-11 08:25:40 -0500

    Set PATH and updating guix channel repo

--- guix-config/arbn/modules/service-lists.scm
+++ guix-config/arbn/modules/service-lists.scm
@@ -49,7 +49,7 @@
                                              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
                                      (channel
                                       (name 'arbn)
-                                      (url "https://github.com/aurbsn/arbn-guix-channel")
+                                      (url "https://git.disroot.org/arbn/arbn-guix-channel")
                                       (branch "main")
                                       (introduction
                                        (make-channel-introduction
