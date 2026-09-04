01fc7d7d1a49625eb90069f9a0d26bc61ed329f0
Austin Robinson <arbn@arbn.nyc>
2026-09-04 16:35:04 -0400

    Adds channels.scm for bootstrapping

---
+++ guix-config/channels.scm
@@ -0,0 +1,11 @@
+(cons* 
+ (channel
+  (name 'arbn)
+  (url "https://github.com/aurbsn/arbn-guix-channel.git")
+  (branch "main")
+  (introduction
+   (make-channel-introduction
+    "3cc6977711fa11f94760bfd97be6723e56a51222"
+    (openpgp-fingerprint
+     "FD2F 077F 9BD6 CBB3 471A  D63A 3029 8DA2 EEB5 DE28"))))
+ %default-channels)
