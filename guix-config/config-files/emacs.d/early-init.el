a11bb4ba81eb8bff5b88c883d9522b8f748ed291
Austin Robinson <arbn@arbn.nyc>
2026-09-18 01:25:43 -0400

    Disable 'package conditionallty for pre-Guix support on foreign distros like macOS

--- guix-config/config-files/emacs.d/early-init.el
+++ guix-config/config-files/emacs.d/early-init.el
@@ -1,1 +1,6 @@
-(setq package-enable-at-startup nil)
+(if (file-exists-p "~/.guix-home/profile")
+    (setq package-enable-at-startup nil)
+  (progn
+    (require 'package)
+    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+    (setq use-package-always-ensure t)))
