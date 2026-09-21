879e501b66479851130ff285952dd78e58a14541
Austin Robinson <arbn@arbn.nyc>
2026-09-20 23:57:46 -0400

    eat + eshell

--- guix-config/config-files/emacs.d/customizations/setup-eshell.el
+++ guix-config/config-files/emacs.d/customizations/setup-eshell.el
@@ -1,3 +1,6 @@
 ;; -*- lexical-binding: t; -*-
+(use-package eat
+  :ensure t
+  :hook (eshell-load . eat-eshell-mode))
 (setq eshell-visual-subcommands '(("git" "log" "diff" "show") ("guix" "search")))
 (setq eshell-destroy-buffer-when-process-dies 't)
